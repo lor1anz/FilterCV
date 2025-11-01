993d7b762942d1286ae26da1ad318fa5b88d59bb
Pavel Rozhkov <mrkraft500@gmail.com>
2025-11-01 16:01:07 +0300

    NEW: qt: simple window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
+#include <QApplication>
+#include <QWidget>
 
 #include "globals.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-  printf ("Hello world!\n");
-  return 0;
+  QApplication app (argc, argv);
+
+  QWidget window;
+  window.setWindowTitle ("FilterCV");
+  window.resize (400, 300);
+  window.show ();
+
+  return app.exec ();
 }
