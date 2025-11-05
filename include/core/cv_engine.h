c9253d743b51d2004cf8e0aca01ed56b1923fd81
pavel.rozhkov <mrkraft500@gmail.com>
2025-11-05 23:39:10 +0300

    ENH: core: add image, video and camera capture

--- include/core/cv_engine.h
+++ include/core/cv_engine.h
@@ -13,13 +13,25 @@ namespace core
 class cv_engine
 {
 public:
+  enum class source { image, video, camera };
+
   cv_engine () = default;
-  ~cv_engine () = default;
+  ~cv_engine () { close (); }
 
   cv_engine (const cv_engine &) = delete;
   cv_engine &operator= (const cv_engine &) = delete;
   cv_engine (cv_engine &&) = delete;
   cv_engine &operator= (cv_engine &&) = delete;
+
+  // sources
+  void set_source (source s);
+  void set_test_image (const cv::Mat &bgr);
+  void set_test_video_file (const QString &path);
+  void set_camera_index (int index);
+
+  bool open ();
+  void close ();
+  bool grab ();
 
   void set_source_image (const cv::Mat &bgr);
   void clear_filters ();
@@ -29,7 +41,14 @@ public:
   QImage process ();
 
 private:
-  cv::Mat source_bgr;
+  source src = source::image;
+  cv::Mat test_bgr;
+  QString video_path;
+  int camera_index = 0;
+  cv::VideoCapture capture;
+
+  cv::Mat current_bgr;
+
   std::vector<std::shared_ptr<filters::filter>> pipeline;
 };
 
--- src/core/cv_engine.cpp
+++ src/core/cv_engine.cpp
@@ -1,13 +1,93 @@
 #include "core/cv_engine.h"
 
+#include <QDebug>
+
+#include <opencv2/videoio.hpp>
+
 #include "gui/utils.h"
 
 namespace core
 {
 
-void cv_engine::set_source_image (const cv::Mat &bgr) 
+void cv_engine::set_source (source s) { src = s; }
+void cv_engine::set_test_image (const cv::Mat &bgr) { test_bgr = bgr.clone (); }
+void cv_engine::set_test_video_file (const QString &path) { video_path = path; }
+void cv_engine::set_camera_index (int index) { camera_index = index; }
+
+bool cv_engine::open ()
 {
-  source_bgr = bgr.clone();
+  close ();
+  if (src == source::video)
+    {
+      capture.open (video_path.toStdString ());
+      if (!capture.isOpened ())
+        {
+          qWarning () << "Cannot open video:" << video_path; 
+          return false;
+        }
+      return true;
+    }
+  else if (src == source::camera)
+    {
+      if (!capture.open (camera_index, cv::CAP_ANY)) 
+        {
+          qWarning () << "Cannot open camera" << camera_index;
+          return false;
+        }
+      return true;
+    }
+  else
+    {
+      // image doesn't need to be open
+    }
+  return true;
+}
+
+void cv_engine::close ()
+{
+  if (capture.isOpened ())
+    capture.release ();
+}
+
+bool cv_engine::grab ()
+{
+  switch (src)
+    {
+      case source::image:
+        {
+          if (test_bgr.empty ())
+            return false;
+          current_bgr = test_bgr.clone ();
+          return true;
+        }
+
+      case source::video:
+      case source::camera:
+        {
+          if (!capture.isOpened ())
+            {
+              if (!open ())
+                return false;
+            }
+          cv::Mat frame;
+          if (!capture.read (frame) || frame.empty ())
+            {
+              if (src == source::video)
+                {
+                  if (!capture.read (frame) || frame.empty ())
+                    return false;
+                }
+              else
+                {
+                  return false;
+                }
+            }
+          current_bgr = std::move (frame);
+          return true;
+        }
+    }
+
+  return false;
 }
 
 void cv_engine::clear_filters ()
@@ -35,13 +115,16 @@ std::shared_ptr<filters::filter> cv_engine::find_filter (const char *id)
 
 QImage cv_engine::process () 
 {
-  if (source_bgr.empty ()) 
+  if (current_bgr.empty ()) 
     return {};
 
-  cv::Mat src = source_bgr;
+  cv::Mat src = current_bgr;
   cv::Mat dst;
   for (auto &filter : pipeline) 
     {
+      if (!filter)
+        continue;
+
       filter->apply (src, dst);
       if (dst.data != src.data) 
         src = dst;
